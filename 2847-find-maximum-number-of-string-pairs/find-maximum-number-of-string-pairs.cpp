4bf9a709c7c5224fc3a6769f030a9f6fc9e3f7ff
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-11 17:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (33.11%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -4,19 +4,14 @@ public:
         //using unorderd set 
         int n = words.size();
         unordered_set<string>s;
-        for(int i=0;i<n;i++){
-            s.insert(words[i]);
-        }
-
         int count=0;
         for(int i=0;i<n;i++){
             string rev = words[i];
             reverse(rev.begin(),rev.end());
-            if(words[i]==rev) continue;//agr dono char same hai tb 
             if(s.find(rev)!=s.end()){//set m ele find krna
                 count++;
-                s.erase(words[i]);
             }
+            else s.insert(words[i]);
         }
         return count;
     }
