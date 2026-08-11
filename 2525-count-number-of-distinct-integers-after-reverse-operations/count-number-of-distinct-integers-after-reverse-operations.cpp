e04bfb7feb4d33fd58e06f28ce36cb71c6a5b0dd
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-11 15:56:14 +0530

    Time: 182 ms (55.52%) | Memory: 110.6 MB (53.18%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    void reverse(int num,vector<int>&ans,int i){
+    int reverse(int num,vector<int>&nums){
         int rev = 0;
         while(num!=0){
             int rem = num%10;
             rev = rev*10 + rem;
             num = num/10;
         }
-        ans[i] = rev;
-        i++;   
+        return rev;
     }
     int countDistinctIntegers(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(2*n);
-        for(int i=0;i<n;i++) ans[i] = nums[i];
-        for(int i=0;i<n;i++){
-            reverse(nums[i],ans,i+n); 
-        }
-        
         unordered_set<int>s;
-        for(int i=0;i<2*n;i++){
-            s.insert(ans[i]);
+        for(int i=0;i<n;i++){
+           int rev =  reverse(nums[i],nums);
+           s.insert(rev);
+           s.insert(nums[i]); 
         }
         return s.size();
     }
