cb1dee4e5cc8c761527f384ef8d50cc792b45688
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-12 06:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // usinh hash map
         if(s.length()!=t.length()) return false;
         unordered_map<char,int>m1;
         unordered_map<char,int>m2;
 
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){//calculation of frequency of char at s
             m1[s[i]]++;
         }
 
-        for(int i=0;i<t.length();i++){
+        for(int i=0;i<t.length();i++){//calculation of frequency of char at t
             m2[t[i]]++;
         }
 
         for(auto p:m1){
             char ch1 = p.first;
             int freq1 = p.second;
-            if(m2.find(ch1)!=m2.end()){//ele find hogya
-                int freq2 = m2[ch1];//check the frequency
-                if(freq1!=freq2) return false;
 
+            if(m1.find(ch1)!=m1.end()){//ele found now check freq
+                if(freq1!=m2[ch1]) return false;
             }
-            else return false;
+
+            else return false;//element not found
         }
 
         return true;
-
-
     }
 };
