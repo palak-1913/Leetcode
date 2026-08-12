a407bf87a45b08fa04562291eb8a98a34555c4ba
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-12 21:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (7.87%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        //make an map which will store the frequency of arr1
+        unordered_map<int,int>mp;
         int n = arr1.size();
         int m = arr2.size();
+        for(int i=0;i<n;i++){
+            mp[arr1[i]]++;//freq clculation
+        }
+
         vector<int>ans;
         for(int i=0;i<m;i++){
-            int num = arr2[i];
-            for(int j=0;j<n;j++){
-                if(arr1[j]==num) {
-                    ans.push_back(arr1[j]);
-                    arr1[j] = INT_MAX;//woh ele rakh diya hai toh usse hata do 
-                }
+            int val = arr2[i];
+            while(mp.find(val)!=mp.end() && mp[val]!=0){//ele mil gya aur freq 0 nhi hai toh usko ans m push kr do
+                ans.push_back(val);
+                mp[val]--;
             }
+            mp.erase(val);
         }
-        //place remaning
-        sort(arr1.begin(),arr1.end());
-        for(int i=0;i<n;i++){
-            if(arr1[i]!=INT_MAX) ans.push_back(arr1[i]);
+
+        vector<int>rem;
+        for(auto p:mp){
+            int val = p.first;
+            int freq = p.second;
+            while(freq!=0) {
+                rem.push_back(val);
+                freq--;
+            }
+            
         }
+
+        sort(rem.begin(),rem.end());
+
+        for(int i=0;i<rem.size();i++) ans.push_back(rem[i]);
+
         return ans;
-
-
     }
 };
