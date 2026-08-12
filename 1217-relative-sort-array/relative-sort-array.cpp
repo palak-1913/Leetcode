5c57ba304dca3013de02237c7db6e26c57649694
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-12 18:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (67.26%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -9,14 +9,14 @@ public:
             for(int j=0;j<n;j++){
                 if(arr1[j]==num) {
                     ans.push_back(arr1[j]);
-                    arr1[j] = -100;//woh ele rakh diya hai toh usse hata do 
+                    arr1[j] = INT_MAX;//woh ele rakh diya hai toh usse hata do 
                 }
             }
         }
         //place remaning
         sort(arr1.begin(),arr1.end());
         for(int i=0;i<n;i++){
-            if(arr1[i]!=-100) ans.push_back(arr1[i]);
+            if(arr1[i]!=INT_MAX) ans.push_back(arr1[i]);
         }
         return ans;
 
