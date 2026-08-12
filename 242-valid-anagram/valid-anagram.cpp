98d65dbd6570616838aa693dfc802ca43412580e
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-12 06:20:19 +0530

    Time: 6 ms (22.11%) | Memory: 9.9 MB (26.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // Method-3
+
         if(s.length()!=t.length()) return false;
         unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
 
         for(int i=0;i<s.length();i++){//calculation of frequency of char at s
             m1[s[i]]++;
         }
 
-        for(int i=0;i<t.length();i++){//calculation of frequency of char at t
-            m2[t[i]]++;
-        }
-
-        for(auto p:m1){
-            char ch1 = p.first;
-            int freq1 = p.second;
-
-            if(m1.find(ch1)!=m1.end()){//ele found now check freq
-                if(freq1!=m2[ch1]) return false;
+        for(int i=0;i<t.length();i++){
+            char ch = t[i];
+            if(m1.find(ch)!=m1.end()){//ele find ho jata hai 
+                m1[ch]--;//freq dec kro 
+                if(m1[ch]==0) m1.erase(ch); //jaise hi freq 0 hui usse map se hata do
             }
 
-            else return false;//element not found
+            else return false;//ele find hi nhi hua
         }
 
-        return true;
+        if(m1.size()) return false;
+        else return true;
+
+        
     }
 };
