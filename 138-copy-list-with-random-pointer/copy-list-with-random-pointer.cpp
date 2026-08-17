098711cbaefa46bb5cc8d072e9cf2906e67dab24
Palak Pandey <218497249+palak-1913@users.noreply.github.com>
2026-08-17 18:49:41 +0530

    Time: 4 ms (68.92%) | Memory: 15.1 MB (71.82%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,39 +16,60 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {//create dummy node
-        Node*dummy = new Node(0);
-        Node*temp = head;
-        Node*tempc = dummy;
-        while(temp!=NULL){
-            Node*a = new Node(temp->val);
-            tempc->next = a;
-            tempc = tempc->next;
-            temp = temp->next;
-        }
-        Node*a = head;
-        Node*b = dummy->next;
-        //make a map of org , dummy node.
-        unordered_map<Node*,Node*>m;
-        Node*tempa = a;
-        Node*tempb = b;
-        while(tempa!=NULL && tempb!=NULL){
-            m[tempa] = tempb;
-            tempa = tempa->next;
-            tempb = tempb->next;
+    Node* copyRandomList(Node* head) {
+        //create a dummy node with any random pointer 
+        Node*t = head;
+        Node*dummy = new Node(INT_MAX);
+        Node*td = dummy;
+        while(t!=NULL){
+            Node*a = new Node(t->val);
+            td->next = a;
+            t = t->next;
+            td = td->next;
         }
 
-        for(auto x:m){
-            Node*o = x.first;
-            Node*d = x.second;
-            if(o->random!=NULL){
-                Node*oRandom = o->random;
-                Node*dupRandom = m[o->random];
-                d->random = dupRandom;
-            }
+        // now merge the LL(alternate connection)
+        td = dummy;
+        Node*b=dummy->next;
+        Node*a=head;
+        while(a!=NULL){
+            td->next = a;
+            a = a->next;
+            td = td->next;
+            td->next = b;
+            b = b->next;
+            td = td->next;
+        }
+        
+        dummy = dummy->next;
+        a = dummy;
+        b = head;
+        while(a!=NULL){
+            b = a->next;
+            if(a->random!=NULL)b->random = a->random->next;
+            a = a->next->next;
         }
 
-        return b;
+        //now remove the connections
+        Node*d1 = new Node(INT_MAX);
+        Node*t1 = d1;
+        Node*d2 = new Node(INT_MAX);
+        Node*t2 = d2;
+        td = dummy;
+        while(td!=NULL){
+            t1->next = td;
+            td = td->next;
+            t1 = t1->next;
 
+            t2->next = td;
+            t2 = t2->next;
+            td = td->next;
+        }
+        t1->next=NULL;
+        t2->next=NULL;
+        d2 = d2->next;
+        d1 = d1->next;
+        return d2;
+         
     }
 };
